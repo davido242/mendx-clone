38e47d499fd2a53edec4ad952c39c8f1ce8c544f
davido242 <davidsarka242@gmail.com>
2025-05-08 23:45:09 +0100

    Fixxed Image tag

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -38,7 +38,7 @@ export default function header() {
         <div className="container mx-auto px-8 py-2 flex justify-between items-center">          
           <HamburgerButton />          
           <div className="cursor-pointer" onClick={handleHomePage}>
-            <Image src="/assets/images/mendexLogo.jpg" alt="Mendex logo" width="45" height="45" />
+            <img src="/assets/images/mendexLogo.jpg" alt="Mendex logo" width="45" height="45" />
           </div>
           <div>
             {name == "" ? (
